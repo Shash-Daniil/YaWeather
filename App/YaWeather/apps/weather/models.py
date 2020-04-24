998255185d0b0ff74579472de158a46b5f2be936
NoNameee <granium999@gmail.com>
2020-04-24 21:27:30 +0300

    hi

--- App/YaWeather/apps/weather/models.py
+++ App/YaWeather/apps/weather/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+import datetime
 
-# Create your models here.
+class Author(models.Model):
+	"""пост"""
+	id = models.CharField("id поста", max_length = 30, primary_key = True)
+	name = models.CharField("Имя поста", max_length = 50)
+	text = models.TextField("Текст поста")
+	date = models.DateTimeField(default=datetime.datetime.now())
